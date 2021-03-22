19723963700dcb5471aa1338a12d08e8a89a72cd
giulianaleon <44512883+giulianaleon@users.noreply.github.com>
2021-03-22 15:36:13 -0300

    Add files via upload

--- src/src/Personagem.java
+++ src/src/Personagem.java
@@ -1,14 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package src;
 
-/**
- *
- * @author indra
- */
 public abstract class Personagem {
-    
+
+	private int poder;
+	private int vida;
+	private int defesa;
+	private int ataque;
+	//private int nivel;
+	
+	public Personagem(int p, int v, int d, int a) {
+		this.poder = p;
+		this.ataque = a;
+		this.defesa = d;
+		this.vida = v;
+	}
+
+	public int getPoder() {
+		return poder;
+	}
+
+	public void setPoder(int poder) {
+		this.poder = poder;
+	}
+
+	public int getVida() {
+		return vida;
+	}
+
+	public void setVida(int vida) {
+		this.vida = vida;
+	}
+
+	public int getDefesa() {
+		return defesa;
+	}
+
+	public void setDefesa(int defesa) {
+		this.defesa = defesa;
+	}
+
+	public int getAtaque() {
+		return ataque;
+	}
+
+	public void setAtaque(int ataque) {
+		this.ataque = ataque;
+	}
+	
+	//Métodos
+	
 }
--- src/src/Protagonista.java
+++ src/src/Protagonista.java
@@ -1,41 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package src;
-
 import src.exceptions.SemDinheiroException;
 
-/**
- *
- * @author indra
- */
-public class Protagonista extends Personagem {
-    
-    public void comprarCura()throws SemDinheiroException{
-       double dinheiro=0.0;//variavel provis贸ria
-       int cura=0; //variavel provis贸ria
-       if(dinheiro > 2.5){
-           dinheiro-=2.5;
-           cura++;
-       }else throw new SemDinheiroException(2.5);
-    }
-    
-    public void aumentarAtaque() throws SemDinheiroException{
-        double dinheiro=0.0;//variavel provis贸ria
-        int ataque=0; //variavel provis贸ria
-        if(dinheiro > 3){
-            dinheiro-=3;
-            ataque++;
-        }else throw new SemDinheiroException(3);
-    }
-    public void aumentarDefesa() throws SemDinheiroException{
-        double dinheiro=0.0;//variavel provis贸ria
-        int defesa=0; //variavel provis贸ria
-        if(dinheiro > 3){
-            dinheiro-=3;
-            defesa++;
-        }else throw new SemDinheiroException(3);
-    }
+public class Protagonista extends Personagem{
+	
+	private int dinheiro;
+	private int cura;
+	
+	public Protagonista(int p, int v, int d, int a, int din, int c) {
+		super(p, v, d, a);
+		this.dinheiro = din;
+		this.cura = c;
+	}
+
+	public int getDinheiro() {
+		return dinheiro;
+	}
+
+	public void setDinheiro(int dinheiro) {
+		this.dinheiro = dinheiro;
+	}
+
+	public int getCura() {
+		return cura;
+	}
+
+	public void setCura(int cura) {
+		this.cura = cura;
+	}
+	
+	 public void comprarCura()throws SemDinheiroException{
+	       double dinheiro=0.0;//variavel provis髍ia
+	       int cura=0; //variavel provis髍ia
+	       if(dinheiro > 2.5){
+	           dinheiro-=2.5;
+	           cura++;
+	       }else throw new SemDinheiroException(2.5);
+	    }
+	    
+	    public void aumentarAtaque() throws SemDinheiroException{
+	        double dinheiro=0.0;//variavel provis髍ia
+	        int ataque=0; //variavel provis髍ia
+	        if(dinheiro > 3){
+	            dinheiro-=3;
+	            ataque++;
+	        }else throw new SemDinheiroException(3);
+	    }
+	    public void aumentarDefesa() throws SemDinheiroException{
+	        double dinheiro=0.0;//variavel provis髍ia
+	        int defesa=0; //variavel provis髍ia
+	        if(dinheiro > 3){
+	            dinheiro-=3;
+	            defesa++;
+	        }else throw new SemDinheiroException(3);
+	    }
+	}
+	
+	
+
 }
